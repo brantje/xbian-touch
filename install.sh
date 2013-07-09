8429df1b3b5f98d4f350596faf06372ddfa79e89
brantje <brantje@gmail.com>
2013-07-09 13:53:15 +0000

    Small bug fix

--- install.sh
+++ install.sh
@@ -52,5 +52,5 @@ echo "Moving config..."
 sudo mv uimapper.conf /etc/init
 echo "Cleaning up..."
 rm uimapper.tar.gz
-rm tslib_1-1_armhf.deb > /dev/null
+rm tslib_1-1_armhf.deb > /dev/null #Silent error
 sudo reboot
